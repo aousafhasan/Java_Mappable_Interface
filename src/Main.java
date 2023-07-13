b438510e9416ddace68d5a719bae635e2341b3cd
Aousaf Hasan <aousaf.hasan@gmail.com>
2023-07-13 00:47:36 -0400

    Used scanner to take user's proper dob and print the correct age

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String usersDOB = "1999";
+        int dob = Integer.parseInt(usersDOB);
+        int currYear = 2023;
+        System.out.println("Enter your dob");
+        usersDOB = sc.nextLine();
+        while (true){
+            if((currYear - Integer.parseInt(usersDOB) < 0) || (currYear - Integer.parseInt(usersDOB) > 120) )
+            {   System.out.println("Please enter proper DOB");
+                usersDOB = sc.nextLine();
+            }
+            else {
+                break;
+            }
+        }
+        System.out.println("Your age is " + (currYear-Integer.parseInt(usersDOB)) + " years");
     }
 }
